42f9401c3b4d75bf1423316e598b26e379631ee6
Himanshu <kumarhimanshu2219@gmail.com>
2023-11-11 19:44:57 +0530

    db-connect

---
+++ server/database/index.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const { env_config } = require("../config/config.env");
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(env_config.MONGODB_URL);
+    console.log("Connected to the database");
+  } catch (err) {
+    console.error("Error while connecting to the database");
+    console.error(err.message);
+  }
+};
+
+module.exports = { connectToDatabase };
--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const { env_config } = require("./config/config.env");
+const { connectToDatabase } = require("./database");
 
 const app = express();
 const PORT = env_config.PORT || 8080;
+
+(async () => await connectToDatabase())();
 
 app.get("/", (req, res) => {
   res.send("hi");
